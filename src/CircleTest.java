eb8792fae7c71d663ba37ee54961762f26980c99
hansen <1979742361@qq.com>
2021-12-16 17:22:18 +0800

    circle commit2

--- src/CircleTest.java
+++ src/CircleTest.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+
 /**
  * @author: hansen
  * @date: 2021 - 12 - 16 - 17:13
@@ -8,9 +10,16 @@
 public class CircleTest
 {
     public static void main(String[] args){
-
+        Circle c1=new Circle();
+        c1.radius=2.1;
+        System.out.println(c1.findArea());
     }
 }
 class Circle{
-
+    double radius;
+    //方法
+    public double findArea(){
+        double area=Math.PI*radius*radius;
+        return area;
+    }
 }
